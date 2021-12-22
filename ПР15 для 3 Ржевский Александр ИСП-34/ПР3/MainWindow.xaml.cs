e6448528e69a789de3e7c5ed42dd26a357095003
Александр Ржевский ИСП-34 <95027695+20RA03@users.noreply.github.com>
2021-12-22 18:23:49 +0300

    Add files via upload

---
+++ ПР15 для 3 Ржевский Александр ИСП-34/ПР3/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ПР3
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private Array _myArray = new Array(3, 3);
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                rez.Text = _myArray.CountOfNumbers();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Ошибка");
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            _myArray.Fill();
+            dataGrid.ItemsSource = _myArray.ToDataTable().DefaultView;
+        }
+
+        private void Info_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Работу выполнил студент группы ИСП-34 Ржевский Александр. Дана матрица размера M × N. В каждом столбце матрицы найти максимальный элемент.", "Разработчик", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+        private void Clear_Click(object sender, RoutedEventArgs e)
+        {
+            dataGrid.ItemsSource = null;
+            dataGrid.Items.Clear();
+            rez.Clear();
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
